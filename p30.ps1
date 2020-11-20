de56619078449e3007a91473cd2355324a04ae29
Palanikarthikeyan <abpalanikarthik@gmail.com>
2020-11-20 16:30:36 +0530

    Update p30.ps1

--- p30.ps1
+++ p30.ps1
@@ -1,3 +1,4 @@
-﻿# Get-Process chrome|where-object {$_.cpu -gt 15}
+#  Author: Mr.Palani Karthikeyan 
+# Get-Process chrome|where-object {$_.cpu -gt 15}
 
 Get-Process|where-object {$_.Name -eq "chrome" -and $_.CPU -gt 15}
